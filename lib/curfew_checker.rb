dcf9a720c03bf02e82ffb0d089877cbd83587a5a
Gabbypace <golfmarch03@gmail.com>
2018-07-30 13:44:40 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,9 +1,38 @@
-def simple_curfew_checker(time)
-  # code goes here
+  `def simple_curfew_checker(time)
+  if time >= 11
+     "You're in trouble! Better get home quick!"
+  else 
+    "Keep having fun"
+  end
 end
 
+simple_curfew_checker(12)
+
 def curfew_checker(time)
+  if time >= 11
+    "You're in trouble!Better get home quick!"
+    elsif time < 11 
+    "keep having fun"
+  end  
+end
+
+def complex_curfew_checker(time)
+  if time == 11 
+    "Time to apparate!"
+    elsif time > 11 
+   puts "You're in trouble!Better get home quick!"
+    else  
+  curfew = 11 
+  time_left = curfew - time 
+  puts "you have #{time_left} hour(s) left to keep having fun!"
+  end
+end 
+
+complex_curfew_checker(12)
   # code goes here
+
+def curfew_checker(time)
+  # code goes ere
 end
 
 def complex_curfew_checker(time)
---
+++ lib/frihash.rb
@@ -0,0 +1,9 @@
+desserts = {
+  :cookies => "chocolate chip",
+  :brookies => "brookie",
+  :cake => "red velvet",
+  :ice_cream => "chocolate"
+}
+
+
+
